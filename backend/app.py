86f15e472965c669c2bfb14985aa5892f517fe05
BhavyaSre-git <bhavyabalaram.mettupalli@gmail.com>
2026-08-11 16:24:07 +0530

    Dockerize full stack application

--- backend/app.py
+++ backend/app.py
@@ -361,4 +361,4 @@ def update_attendance(student_id):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_DEBUG', '1') == '1'
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=5000, debug=True)
